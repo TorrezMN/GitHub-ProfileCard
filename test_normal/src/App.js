c75e4734a1080b1653b52fca44cac57c78bea38b
TorrezMN <torrez.mn@gmail.com>
2022-06-27 20:55:46 -0400

    FIRST commit.

---
+++ test_normal/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+// Imports
+import '@torrez_mn/gh-card';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+	  <gh-card 
+		gh-user='yogonza524'
+		gh-mode='normal' 
+		gh-primary-color='rgba(0,23,255,.5)'
+		gh-secondary-color='whitesmoke'
+		/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
